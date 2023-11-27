4118f1f7d240b42eef8613a62cc21335c00b9fdd
zxl <2729173112@qq.com>
2023-11-27 17:09:38 +0800

    SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+// 启用SSR
+// import vercel from "@astrojs/vercel/serverless";
+// output: "server",
+// adapter: vercel()
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [preact()],
+});
+
+// import { defineConfig } from 'astro/config';
+
+// export default defineConfig({
+//   output: 'static'
+// })
