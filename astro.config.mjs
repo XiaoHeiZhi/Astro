aa9371a17a6e5ff1c8638cb2ac0c904db51ef6ec
zxl <2729173112@qq.com>
2023-11-27 18:00:28 +0800

    实验SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
+import vercel from "@astrojs/vercel/serverless";
 // 启用SSR
-// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
